eac4b04440d56a11abdf9e9fe01ac3d6e9b2d8e8
Михайлов Сергей <marrog1@mail.ru>
2016-10-07 11:52:25 +0300

    initial commit

---
+++ ViewLearning/app/src/main/java/com/marrog/viewlearning/ViewLearning.java
@@ -0,0 +1,13 @@
+package com.marrog.viewlearning;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ViewLearning extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_view_learning);
+    }
+}
