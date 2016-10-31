39bb9467e863454db757276135c56440e5609052
Михайлов Сергей <marrog1@mail.ru>
2016-10-31 16:05:34 +0300

    trainView home work

--- ViewLearning/app/src/main/java/com/marrog/viewlearning/ViewLearning.java
+++ ViewLearning/app/src/main/java/com/marrog/viewlearning/ViewLearning.java
@@ -8,6 +8,6 @@ public class ViewLearning extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_view_learning);
+        setContentView(R.layout.activity_view_learning_homework);
     }
 }
